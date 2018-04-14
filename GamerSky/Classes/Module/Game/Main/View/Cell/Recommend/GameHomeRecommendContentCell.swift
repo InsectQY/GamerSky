6ab516b36dd18b51eab925f03c1c230f19ce8adb
Insect <2562249572@qq.com>
2018-04-15 01:05:27 +0800

    新游戏推荐分页效果实现

--- GamerSky/Classes/Module/Game/Main/View/Cell/Recommend/GameHomeRecommendContentCell.swift
+++ GamerSky/Classes/Module/Game/Main/View/Cell/Recommend/GameHomeRecommendContentCell.swift
@@ -12,12 +12,16 @@ class GameHomeRecommendContentCell: UITableViewCell, NibReusable {
     
     static let cellHeight: CGFloat = ScreenHeight * 0.28
     
+    //分页序号
+    private var selectedIndex: Int = 0
     /// cell 之间间距
     private let kItemMargin: CGFloat = 5
     /// 左右间距
     private let kEdge: CGFloat = 10
     /// 每行最大列数
     private let kMaxCol: CGFloat = 1
+    /// 每行最大列数
+    private let kFooterWidth: CGFloat = ScreenWidth * 0.25
     /// cell 宽度
     private var kItemW: CGFloat {
         return (ScreenWidth - (2 * kEdge) - ((kMaxCol - 1) * kItemMargin)) / kMaxCol
@@ -46,9 +50,9 @@ class GameHomeRecommendContentCell: UITableViewCell, NibReusable {
     private func setUpCollectionView() {
         
         flowLayout.itemSize = CGSize(width: kItemW, height: GameHomeRecommendContentCell.cellHeight)
-        flowLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, kEdge)
+        flowLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, kItemMargin)
         flowLayout.minimumLineSpacing = kItemMargin
-        flowLayout.footerReferenceSize = CGSize(width: ScreenWidth * 0.25, height: GameHomeRecommendContentCell.cellHeight)
+        flowLayout.footerReferenceSize = CGSize(width: kFooterWidth, height: GameHomeRecommendContentCell.cellHeight)
         collectionView.contentInset = UIEdgeInsetsMake(0, kEdge, 0, kEdge)
         collectionView.register(cellType: GameHomeRecommendCell.self)
         collectionView.register(supplementaryViewType: GameRecommendFooterView.self, ofKind: UICollectionElementKindSectionFooter)
@@ -77,5 +81,29 @@ extension GameHomeRecommendContentCell: UICollectionViewDelegate {
         
         let footer = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionFooter, for: indexPath, viewType: GameRecommendFooterView.self)
         return footer
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+extension GameHomeRecommendContentCell: UIScrollViewDelegate {
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let x = targetContentOffset.pointee.x
+        let pageWidth = kItemW + kItemMargin
+        let movedX = x - pageWidth * CGFloat(selectedIndex)
+        if movedX < -kFooterWidth {
+            // Move left
+            selectedIndex -= 1
+        } else if movedX > kFooterWidth {
+            // Move right
+            selectedIndex += 1
+        }
+        if abs(velocity.x) >= 2 {
+            targetContentOffset.pointee.x = pageWidth * CGFloat(selectedIndex) - kEdge
+        } else {
+            targetContentOffset.pointee.x = scrollView.contentOffset.x
+            scrollView.setContentOffset(CGPoint(x: pageWidth * CGFloat(selectedIndex) - kEdge, y: scrollView.contentOffset.y), animated: true)
+        }
     }
 }
