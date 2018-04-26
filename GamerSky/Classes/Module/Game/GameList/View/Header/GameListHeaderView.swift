2d9746c19bc190b79ee54c5f08672e340c52bdc7
InsectQY <2562249572@qq.com>
2018-04-26 16:36:21 +0800

    找游戏排行方式界面搭建

---
+++ GamerSky/Classes/Module/Game/GameList/View/Header/GameListHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  GameListHeaderView.swift
+//  GamerSky
+//
+//  Created by InsectQY on 2018/4/26.
+//Copyright © 2018年 engic. All rights reserved.
+//
+
+import UIKit
+
+class GameListHeaderView: UICollectionReusableView, NibReusable {
+
+    // MARK: - LazyLoad
+    
+    
+    // MARK: - inital
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
