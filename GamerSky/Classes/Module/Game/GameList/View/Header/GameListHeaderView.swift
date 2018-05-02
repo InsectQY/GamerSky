d8a6383fa7127ba2fbeadf71b2d5e847a6bd83ae
InsectQY <2562249572@qq.com>
2018-05-02 17:00:27 +0800

    找游戏热门与时间筛选

--- GamerSky/Classes/Module/Game/GameList/View/Header/GameListHeaderView.swift
+++ GamerSky/Classes/Module/Game/GameList/View/Header/GameListHeaderView.swift
@@ -11,12 +11,23 @@ import UIKit
 class GameListHeaderView: UICollectionReusableView, NibReusable {
 
     static let height: CGFloat = 44
-    // MARK: - LazyLoad
     
+    private var lastSelBtn = UIButton(type: .custom)
     
-    // MARK: - inital
+    // MARK: - IBOutlet
+    @IBOutlet private weak var hotSortBtn: UIButton!
+    @IBOutlet private weak var timeSortBtn: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        sortBtnDidClick(hotSortBtn)
+    }
+    
+    // MARK: - IBAction
+    @IBAction private func sortBtnDidClick(_ sender: UIButton) {
         
+        lastSelBtn.isSelected = false
+        sender.isSelected = true
+        lastSelBtn = sender
     }
 }
