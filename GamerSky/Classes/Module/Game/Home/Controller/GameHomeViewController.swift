d5bd2b6628751422bbcdc6a89544ddaf544746b1
Insect <2562249572@qq.com>
2018-07-07 10:53:23 +0800

    迁移游戏首页模块到 MVVM 架构

--- GamerSky/Classes/Module/Game/Home/Controller/GameHomeViewController.swift
+++ GamerSky/Classes/Module/Game/Home/Controller/GameHomeViewController.swift
@@ -14,27 +14,9 @@ import RxDataSources
 
 class GameHomeViewController: BaseViewController {
     
-//    /// 新游推荐
-//    private lazy var gameSpecialDetail = [GameInfo]()
-//    /// 最近大家都在玩
-//    private lazy var hotGame = [GameInfo]()
-//    /// 即将发售
-//    private lazy var waitSellGame = [GameInfo]()
-//    /// 最期待游戏
-//    private lazy var expectedGame = [GameInfo]()
-//    /// 排行榜
-//    private lazy var rankingGame = [[GameInfo]]()
-//    /// 找游戏
-//    private lazy var gameTags = [GameTag]()
-//    /// 特色专题
-//    private lazy var gameColumn = [GameSpecialList]()
-//    /// section 数据
-//    private lazy var sectionData = [GameHomeSectionModel]()
-    
     private lazy var tableView: UITableView = {
         
         let tableView = UITableView(frame: UIScreen.main.bounds, style: .grouped)
-//        tableView.dataSource = self
         tableView.delegate = self
         tableView.register(cellType: GameHomeRecommendContentCell.self)
         tableView.register(cellType: GameHomeWaitSellContentCell.self)
@@ -87,44 +69,44 @@ extension GameHomeViewController {
         
         dataSource = RxTableViewSectionedReloadDataSource<GameHomeSection>(configureCell: { (ds, tableView, indexPath, item) -> UITableViewCell in
             
-            let row = ds[indexPath.section]
-            if indexPath.section % 2 == 1 { // 1,3,5
-                
-                let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeWaitSellContentCell.self)
-//                cell.sectionType = item.sectionType
-                switch indexPath.section {
-                    
-                case 1:
-                    cell.game = row.hotGame
-                case 3:
-                    cell.game = row.waitSellGame
-                case 5:
-                    cell.game = row.expectedGame
-                default:
-                    break
-                }
-                return cell
-            }
-            
-            if indexPath.section == 0 {
+            switch ds[indexPath] {
+            case let .specialDetailItem(row):
                 
                 let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeRecommendContentCell.self)
-                cell.gameSpecialDetail = row.gameSpecialDetail
+                cell.gameSpecialDetail = row
                 return cell
-            }else if indexPath.section == 2 {
+            case let .hotItem(row, sectionModels):
+                
+                let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeWaitSellContentCell.self)
+                cell.sectionType = sectionModels[indexPath.section].sectionType
+                cell.game = row
+                return cell
+            case let .specialListItem(row):
                 
                 let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeColumnContentCell.self)
-                cell.columnGame = row.gameColumn
+                cell.columnGame = row
                 return cell
-            }else if indexPath.section == 4 {
+            case let .waitSellItem(row,sectionModels):
+                
+                let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeWaitSellContentCell.self)
+                cell.sectionType = sectionModels[indexPath.section].sectionType
+                cell.game = row
+                return cell
+            case let .rankingItem(row):
                 
                 let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeRankingContentCell.self)
-                cell.rankingGame = row.rankingGame
+                cell.rankingGame = row
                 return cell
-            }else {
+            case let .expectedItem(row, sectionModels):
+                
+                let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeWaitSellContentCell.self)
+                cell.sectionType = sectionModels[indexPath.section].sectionType
+                cell.game = row
+                return cell
+            case let .tagItem(row):
                 
                 let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameTagContentCell.self)
-                cell.gameTag = row.gameTags
+                cell.gameTag = row
                 return cell
             }
         })
@@ -142,87 +124,10 @@ extension GameHomeViewController: Refreshable {
         let refreshHeader = initRefreshHeader(tableView) { [weak self] in
             self?.vmOutput.requestCommand.onNext(())
         }
-        
         vmOutput.autoSetRefreshHeaderStatus(header: refreshHeader).disposed(by: rx.disposeBag)
-        
         refreshHeader.beginRefreshing()
     }
 }
-
-//extension GameHomeViewController {
-//    
-//    private func setUpRefresh() {
-//        
-//        tableView.qy_header = QYRefreshHeader(refreshingBlock: { [weak self] in
-//            
-//            guard let `self` = self else {return}
-//            
-//            // sectionHeader 数据
-//            let data = try! Data(contentsOf: Bundle.main.url(forResource: "GameHomeSectionData", withExtension: "plist")!)
-//            self.sectionData = try! PropertyListDecoder().decode([GameHomeSectionModel].self, from: data)
-//            
-//            // 新游推荐
-//            let symbol1 = GameApi.gameSpecialDetail(1, 13)
-//            .cache
-//            .request(objectModel: BaseModel<[GameInfo]>.self)
-//            
-//            // 最近大家都在玩
-//            let symbol2 = GameApi.gameHomePage(1, .hot)
-//            .cache
-//            .request(objectModel: BaseModel<[GameInfo]>.self)
-//            
-//            // 即将发售
-//            let symbol3 = GameApi.gameHomePage(1, .waitSell)
-//            .cache
-//            .request(objectModel: BaseModel<[GameInfo]>.self)
-//            
-//            // 最期待游戏
-//            let symbol4 = GameApi.gameHomePage(1, .expected)
-//            .cache
-//            .request(objectModel: BaseModel<[GameInfo]>.self)
-//            
-//            // 找游戏
-//            let symbol5 = GameApi.gameTags
-//            .cache
-//            .request(objectModel: BaseModel<[GameTag]>.self)
-//            
-//            // 特色专题
-//            let symbol6 = GameApi.gameSpecialList(1)
-//            .cache
-//            .request(objectModel: BaseModel<[GameSpecialList]>.self)
-//            
-//            // 高分榜
-//            let symbol7 = GameApi.gameRankingList(1, .fractions, 0, "all")
-//            .cache
-//            .request(objectModel: BaseModel<[GameInfo]>.self)
-//            .map({[Array($0.result.prefix(5))]})
-//            
-//            // 热门榜
-//            let symbol8 = GameApi.gameRankingList(1, .hot, 0, "all")
-//            .cache
-//            .request(objectModel: BaseModel<[GameInfo]>.self)
-//            .map({[Array($0.result.prefix(5))]})
-//            
-//            Observable
-//            .zip(symbol1, symbol2, symbol3, symbol4, symbol5, symbol6,symbol7, symbol8)
-//            .subscribe(onNext: { (symbol1Data, symbol2Data, symbol3Data, symbol4Data, symbol5Data, symbol6Data,symbol7Data,symbol8Data) in
-//                
-//                self.gameSpecialDetail = symbol1Data.result
-//                self.hotGame = symbol2Data.result
-//                self.waitSellGame = symbol3Data.result
-//                self.expectedGame = symbol4Data.result
-//                self.gameTags = symbol5Data.result
-//                self.gameColumn = symbol6Data.result
-//                self.rankingGame = symbol7Data
-//                self.rankingGame += symbol8Data
-//                self.tableView.reloadData()
-//                self.tableView.qy_header.endRefreshing()
-//            }).disposed(by: self.rx.disposeBag)
-//        })
-//        
-//        tableView.qy_header.beginRefreshing()
-//    }
-//}
 
 extension GameHomeViewController {
     
@@ -240,68 +145,7 @@ extension GameHomeViewController {
         leftItem.text = "游戏"
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftItem)
     }
-    
-    // MARK: - setUpTableHeader
-//    private func setUpTableHeader() {
-//
-//        tableView.tableHeaderView = headerView
-//    }
 }
-
-// MARK: - UITableViewDataSource
-//extension GameHomeViewController: UITableViewDataSource {
-//
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return sectionData.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        if indexPath.section % 2 == 1 { // 1,3,5
-//
-//            let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeWaitSellContentCell.self)
-//            cell.sectionType = sectionData[indexPath.section].sectionType
-//            switch indexPath.section {
-//
-//            case 1:
-//                cell.game = hotGame
-//            case 3:
-//                cell.game = waitSellGame
-//            case 5:
-//                cell.game = expectedGame
-//            default:
-//                break
-//            }
-//            return cell
-//        }
-//
-//        if indexPath.section == 0 {
-//
-//            let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeRecommendContentCell.self)
-//            cell.gameSpecialDetail = gameSpecialDetail
-//            return cell
-//        }else if indexPath.section == 2 {
-//
-//            let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeColumnContentCell.self)
-//            cell.columnGame = gameColumn
-//            return cell
-//        }else if indexPath.section == 4 {
-//
-//            let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameHomeRankingContentCell.self)
-//            cell.rankingGame = rankingGame
-//            return cell
-//        }else {
-//
-//            let cell = tableView.dequeueReusableCell(for: indexPath, cellType: GameTagContentCell.self)
-//            cell.gameTag = gameTags
-//            return cell
-//        }
-//    }
-//}
 
 // MARK: - UITableViewDelegate
 extension GameHomeViewController: UITableViewDelegate {
--- GamerSky/Classes/Module/Game/Home/ViewModel/GameHomeViewModel.swift
+++ GamerSky/Classes/Module/Game/Home/ViewModel/GameHomeViewModel.swift
@@ -13,7 +13,7 @@ import RxDataSources
 
 class GameHomeViewModel: NSObject {
 
-    private let vmDatas = Variable<[([GameInfo], [GameInfo],[GameSpecialList],[GameInfo],[[GameInfo]],[GameInfo], [GameTag], [GameHomeSectionModel])]>([])
+    private let vmDatas = Variable<[GameHomeSection]>([])
     
     struct GameHomeInput {
         
@@ -28,118 +28,179 @@ class GameHomeViewModel: NSObject {
 }
 
 extension GameHomeViewModel: ViewModelable {
-    
+
     typealias Input = GameHomeInput
     typealias Output = GameHomeOutput
-    
+
     func transform(input: GameHomeViewModel.GameHomeInput) -> GameHomeViewModel.GameHomeOutput {
-        
+
         let temp_sections = vmDatas.asObservable().map { (sections) -> [GameHomeSection] in
-            
-            return sections.map({ (symbol1Data, symbol2Data, symbol3Data, symbol4Data, symbol5Data, symbol6Data, symbol7Data, symbol8Data) -> GameHomeSection in
-                
-                return GameHomeSection(gameSpecialDetail: symbol1Data, hotGame: symbol2Data, gameColumn: symbol3Data, waitSellGame: symbol4Data, rankingGame: symbol5Data, expectedGame: symbol6Data, gameTags: symbol7Data, items: symbol8Data)
+            return sections.map({ (item) -> GameHomeSection in
+                return item
             })
         }.asDriver(onErrorJustReturn: [])
-        
+
         let output = GameHomeOutput(sections: temp_sections)
-        
+
         output.requestCommand.subscribe(onNext: { [weak self] in
-            
+
             guard let `self` = self else {return}
             // sectionHeader 数据
             let data = try! Data(contentsOf: Bundle.main.url(forResource: "GameHomeSectionData", withExtension: "plist")!)
             let sectionData = try! PropertyListDecoder().decode([GameHomeSectionModel].self, from: data)
-            
+
             // 新游推荐
             let symbol1 = GameApi.gameSpecialDetail(1, 13)
             .cache
             .request(objectModel: BaseModel<[GameInfo]>.self)
-            
+            .map({GameHomeSection.specialDetailSection([GameHomeItem.specialDetailItem($0.result)])})
+
             // 最近大家都在玩
             let symbol2 = GameApi.gameHomePage(1, .hot)
             .cache
             .request(objectModel: BaseModel<[GameInfo]>.self)
-            
+            .map({GameHomeSection.hotSection([GameHomeItem.hotItem($0.result, sectionData)])})
+
             // 特色专题
             let symbol3 = GameApi.gameSpecialList(1)
             .cache
             .request(objectModel: BaseModel<[GameSpecialList]>.self)
-            
+            .map({GameHomeSection.specialListSection([GameHomeItem.specialListItem($0.result)])})
+
             // 即将发售
             let symbol4 = GameApi.gameHomePage(1, .waitSell)
             .cache
             .request(objectModel: BaseModel<[GameInfo]>.self)
-            
+            .map({GameHomeSection.waitSellSection([GameHomeItem.waitSellItem($0.result, sectionData)])})
+
             // 高分榜
             let symbol5 = GameApi.gameRankingList(1, .fractions, 0, "all")
             .cache
             .request(objectModel: BaseModel<[GameInfo]>.self)
             .map({[Array($0.result.prefix(5))]})
-            
+
             // 热门榜
             let symbol6 = GameApi.gameRankingList(1, .hot, 0, "all")
             .cache
             .request(objectModel: BaseModel<[GameInfo]>.self)
             .map({[Array($0.result.prefix(5))]})
-            
+
             // 最期待游戏
             let symbol7 = GameApi.gameHomePage(1, .expected)
             .cache
             .request(objectModel: BaseModel<[GameInfo]>.self)
-            
+            .map({GameHomeSection.expectedSection([GameHomeItem.expectedItem($0.result, sectionData)])})
+
             // 找游戏
             let symbol8 = GameApi.gameTags
             .cache
             .request(objectModel: BaseModel<[GameTag]>.self)
-            
+            .map({GameHomeSection.tagSection([GameHomeItem.tagItem($0.result)])})
+
             Observable
             .zip(symbol1, symbol2, symbol3, symbol4, symbol5, symbol6,symbol7, symbol8)
             .subscribe(onNext: { (symbol1Data, symbol2Data, symbol3Data, symbol4Data, symbol5Data, symbol6Data,symbol7Data,symbol8Data) in
-                
+
                 var rankingGame = symbol5Data
                 rankingGame += symbol6Data
-                var sectionModels: [([GameInfo], [GameInfo],[GameSpecialList],[GameInfo],[[GameInfo]],[GameInfo], [GameTag], [GameHomeSectionModel])] = []
-                    sectionModels
-                    .append((symbol1Data.result,symbol2Data.result,symbol3Data.result,symbol4Data.result,rankingGame,symbol7Data.result,symbol8Data.result, sectionData))
-                self.vmDatas.value = sectionModels
+                let rankingSection = GameHomeSection
+                .rankingSection([GameHomeItem.rankingItem(rankingGame)])
                 
+                var sectionModels: ([GameHomeSection]) = []
+                sectionModels.append(symbol1Data)
+                sectionModels.append(symbol2Data)
+                sectionModels.append(symbol3Data)
+                sectionModels.append(symbol4Data)
+                sectionModels.append(rankingSection)
+                sectionModels.append(symbol7Data)
+                self.vmDatas.value = sectionModels
+
                 output.refreshStatus.value = .endHeaderRefresh
             }).disposed(by: self.rx.disposeBag)
         }, onError: { (error) in
-            
+
         }).disposed(by: self.rx.disposeBag)
         return output
     }
 }
 
-struct GameHomeSection {
+enum GameHomeSection {
     
     /// 新游推荐
-    var gameSpecialDetail: [GameInfo]
+    case specialDetailSection([GameHomeItem])
     /// 最近大家都在玩
-    var hotGame: [GameInfo]
+    case hotSection([GameHomeItem])
     /// 特色专题
-    var gameColumn: [GameSpecialList]
+    case specialListSection([GameHomeItem])
     /// 即将发售
-    var waitSellGame: [GameInfo]
+    case waitSellSection([GameHomeItem])
     /// 排行榜
-    var rankingGame: [[GameInfo]]
+    case rankingSection([GameHomeItem])
     /// 最期待游戏
-    var expectedGame: [GameInfo]
-    /// 找游戏
-    var gameTags: [GameTag]
+    case expectedSection([GameHomeItem])
+    // 找游戏
+    case tagSection([GameHomeItem])
+}
+
+enum GameHomeItem {
     
-    var items: [Item]
+    /// 新游推荐
+    case specialDetailItem([GameInfo])
+    /// 最近大家都在玩
+    case hotItem([GameInfo], [GameHomeSectionModel])
+    /// 特色专题
+    case specialListItem([GameSpecialList])
+    /// 即将发售
+    case waitSellItem([GameInfo], [GameHomeSectionModel])
+    /// 排行榜
+    case rankingItem([[GameInfo]])
+    /// 最期待游戏
+    case expectedItem([GameInfo], [GameHomeSectionModel])
+    // 找游戏
+    case tagItem([GameTag])
 }
 
 extension GameHomeSection: SectionModelType {
-
-    typealias Item = GameHomeSectionModel
+    
+    typealias Item = GameHomeItem
+    
+    var items: [GameHomeItem] {
+        switch self {
+        case let .specialDetailSection(rows):
+            return rows
+        case let .hotSection(rows):
+            return rows
+        case let .specialListSection(rows):
+            return rows
+        case let .waitSellSection(rows):
+            return rows
+        case let .rankingSection(rows):
+            return rows
+        case let .expectedSection(rows):
+            return rows
+        case let .tagSection(rows):
+            return rows
+        }
+    }
     
     init(original: GameHomeSection, items: [Item]) {
-        
-        self = original
-        self.items = items
+
+        switch original {
+
+        case .specialDetailSection:
+            self = .specialDetailSection(items)
+        case .hotSection:
+            self = .hotSection(items)
+        case .specialListSection:
+            self = .specialListSection(items)
+        case .waitSellSection:
+            self = .waitSellSection(items)
+        case .rankingSection:
+            self = .rankingSection(items)
+        case .expectedSection:
+            self = .expectedSection(items)
+        case .tagSection:
+            self = .tagSection(items)
+        }
     }
 }
